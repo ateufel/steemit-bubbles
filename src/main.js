8ca11f9f90e3fd61cce697dadbe31c05464ce23f
luschn <luschn@gmx.at>
2018-05-14 00:50:36 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import d3 from 'd3';
+
+var svg = d3.select('svg'),
+	diameter = +svg.attr('width'),
+	g = svg.append('g').attr('transform', 'translate(2,2)'),
+	format = d3.format(',d');
+
+var pack = d3.pack()
+	.size([diameter - 4, diameter - 4]);
+
+d3.json('flare.json', function(error, root) {
+  if (error) throw error;
+
+  root = d3.hierarchy(root)
+	  .sum(function(d) { return d.size; })
+	  .sort(function(a, b) { return b.value - a.value; });
+
+  var node = g.selectAll('.node')
+	.data(pack(root).descendants())
+	.enter().append('g')
+	  .attr('class', function(d) { return d.children ? 'node' : 'leaf node'; })
+	  .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+  node.append('title')
+	  .text(function(d) { return d.data.name + '\n' + format(d.value); });
+
+  node.append('circle')
+	  .attr('r', function(d) { return d.r; });
+
+  node.filter(function(d) { return !d.children; }).append('text')
+	  .attr('dy', '0.3em')
+	  .text(function(d) { return d.data.name.substring(0, d.r / 3); });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,87 @@
-const path = require('path');
+const path = require('path'),
+	webpack = require('webpack'),
+	UglifyJS = require('uglifyjs-webpack-plugin'),
+	HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-	mode: "production",
-	entry: "./src/index",
-	output: {
-		path: path.resolve(__dirname, "build"),
-		filename: "bundle.js"
-	},
-	devServer: {
-		contentBase: path.join(__dirname, 'src'),
-		compress: true,
-		historyApiFallback: true,
-		https: false, // true for self-signed, object for cert authority
- 	},
-	module: {
-    	rules: [{
-			test: /\.js?$/,
-			include: [
-				path.resolve(__dirname, "src")
+const HOST = process.env.HOST || 'localhost',
+	PORT = process.env.PORT || 8080;
+
+module.exports = env => {
+	return {
+		entry: './src/main.js',
+		output: {
+			path: path.join(__dirname, 'build'),
+			filename: 'main.js'
+		},
+		//devtool: '#cheap-module-source-map',
+		devServer: {
+			historyApiFallback: true,
+			hot: false,
+			stats: 'errors-only',
+			host: HOST,
+			port: PORT,
+			https: true,
+			inline: true,
+			contentBase: path.join(__dirname, 'build')
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /(node_modules|build*)/,
+					loader: 'babel-loader',
+					options: {
+						//presets and plugins in .babelrc
+					}
+				},
+				{
+					test: /\.css$/,
+					use: [
+						{
+							loader: 'style-loader',
+							options: {
+								//importLoaders: 1,
+								//minimize: true
+							}
+						},
+						{
+							loader: 'css-loader',
+							options: {
+								modules: false
+								//importLoaders: 1,
+								//minimize: true
+							}
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: function () {
+									return [
+										require('postcss-cssnext')
+									];
+								}
+							}
+						}
+					]
+				},
 			]
-			loader: "babel-loader",
-			options: {
-				presets: ["es2015"]
-			}
-		}]
-	}
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': JSON.stringify('production')
+			}),
+			new HtmlWebpackPlugin({
+				inject: false,
+				template: 'htmlWebpackTemplate.ejs',
+				minify: false
+			}),
+			new UglifyJS({
+				uglifyOptions: {
+					compress: {
+						drop_console: true
+					}
+				}
+			})
+		]
+	};
 };
