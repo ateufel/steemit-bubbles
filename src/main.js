189659c4b33b3dd3d933d8d89fef8dfcb7fa4a56
luschn <luschn@gmx.at>
2018-06-06 00:50:28 +0200

    reduce code to simple circles

--- src/main.js
+++ src/main.js
@@ -1,33 +1,55 @@
-import d3 from 'd3';
+import * as d3 from 'd3';
 
-let svg = d3.select('svg'),
-	diameter = +svg.attr('width'),
-	g = svg.append('g').attr('transform', 'translate(2,2)'),
-	format = d3.format(',d');
+let svg = d3.select('svg');
+	//diameter = svg.attr('width'),
+	//g = svg.append('g').attr('transform', 'translate(2,2)'),
+	//format = d3.format(',d');
 
-let pack = d3.pack()
-	.size([diameter - 4, diameter - 4]);
+/*let pack = d3.pack()
+	.size([diameter - 4, diameter - 4]);*/
 
-d3.json('flare.json', function(error, root) {
-	if (error) throw error;
+//this will later be the followers or followings
+const testArray = [
+	{"username": "berndpfeiffer", "SP": 250},
+	{"username": "limesoda", "SP": 7100},
+	{"username": "nissla", "SP": 520}
+];
 
-	root = d3.hierarchy(root)
-		.sum(function(d) { return d.size; })
-		.sort(function(a, b) { return b.value - a.value; });
+const svgContainer = d3.select("body").append("svg")
+	.attr("width", 640)
+	.attr("height", 640);
+const circles = svgContainer.selectAll("circle")
+	.data(testArray)
+	.enter()
+	.append("circle");
 
-	let node = g.selectAll('.node')
-		.data(pack(root).descendants())
-		.enter().append('g')
-		.attr('class', function(d) { return d.children ? 'node' : 'leaf node'; })
-		.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+const circleAttributes = circles
+	/*.attr("cx", function (d) {
+		return d.x_axis;
+	})
+	.attr("cy", function (d) { return d.y_axis; })
+	*/
+	.attr("r", function (d) {
+		return d.SP * 0.01;
+	});
+	//.style("fill", function(d) { return d.color; });
 
-	node.append('title')
-		.text(function(d) { return d.data.name + '\n' + format(d.value); });
+/*let root = d3.hierarchy(testArray)
+	.sum(function(d) {return d.size;})
+	.sort(function(a, b) {return b.value - a.value;});*/
 
-	node.append('circle')
-		.attr('r', function(d) { return d.r; });
+/*let node = g.selectAll('.node')
+	.data(pack(root).descendants())
+	.enter().append('g')
+	.attr('class', (d) => {return d.children ? 'node' : 'leaf node';})
+	.attr('transform', (d) => {return 'translate(' + d.x + ',' + d.y + ')';});*/
 
-	node.filter(function(d) { return !d.children; }).append('text')
-		.attr('dy', '0.3em')
-		.text(function(d) { return d.data.name.substring(0, d.r / 3); });
-});
+/*node.append('title')
+	.text((d) => {return d.username;});
+
+node.append('circle')
+	.attr('r', (d) => {return d.SP;});*/
+
+/*node.filter(function(d) {return !d.children;}).append('text')
+	.attr('dy', '0.3em')
+	.text(function(d) {return d.data.name.substring(0, d.r / 3);});*/
