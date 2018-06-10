0ca97ff8ba0b7739ac1990c42c7802694e0f8667
luschn <luschn@gmx.at>
2018-06-10 11:21:37 +0200

    fix tooltip position

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import {generatePackedCircle} from './services/d3';
 
 const width = 600,
 	height = 600,
+	type = 'followers', //followers or following
 	dataFields = ['vesting_shares', 'sbd'];
 
 (async() => {
 	const followers = await steemGetFollowers('luschn');
 	const accountData = await steemGetAccountData(followers);
+	//prepare data for d3 circle pack
 	const data = {
 		name: 'followers',
 		children: accountData
--- src/services/d3.js
+++ src/services/d3.js
@@ -1,6 +1,31 @@
 import * as d3 from 'd3';
 
 export const generatePackedCircle = (data, valueField, width, height) => {
+	const showTooltip = (d) => {
+		tooltip
+			.transition()
+			.duration(200)
+			.style('opacity', .9);
+		tooltip
+			.html(d.data.name + '<br/>'  + d.data[valueField])
+			.style('left', (d.x) + 'px')
+			.style('top', (d.y - 28) + 'px')
+	};
+
+	const moveTooltip = (d) => {
+		/*tooltip
+			.html(d.data.name + '<br/>'  + d.data[valueField])
+			.style('left', (d3.event.pageX) + 'px')
+			.style('top', (d3.event.pageY - 28) + 'px');*/
+	};
+
+	const hideTooltip = (d) => {
+		tooltip
+			.transition()
+			.duration(500)
+			.style('opacity', 0);
+	};
+
 	//create svg element
 	d3.select('body')
 		.append('svg')
@@ -34,19 +59,9 @@ export const generatePackedCircle = (data, valueField, width, height) => {
 		.attr('cx', (d) => d.x)
 		.attr('cy', (d) => d.y)
 		.attr('r', (d) => d.r)
-		.on('mouseover', (d) => {
-			tooltip.transition()
-				.duration(200)
-				.style('opacity', .9);
-			tooltip	.html(d.data.name + '<br/>'  + d.data[valueField])
-				.style('left', (d3.event.pageX) + 'px')
-				.style('top', (d3.event.pageY - 28) + 'px');
-		})
-		.on('mouseout', (d) => {
-			tooltip.transition()
-				.duration(500)
-				.style('opacity', 0);
-		});
+		.on('mouseover', showTooltip)
+		.on("mousemove", moveTooltip)
+		.on('mouseout', hideTooltip);
 
 	svg
 		.append('text')
@@ -56,17 +71,7 @@ export const generatePackedCircle = (data, valueField, width, height) => {
 			//return d.children === undefined ? `${d.data.name} (${parseFloat(d.data[valueField])})` : '';
 			return d.children === undefined ? d.data.name : '';
 		})
-		.on('mouseover', (d) => {
-			tooltip.transition()
-				.duration(200)
-				.style('opacity', .9);
-			tooltip	.html(d.data.name + '<br/>'  + d.data[valueField])
-				.style('left', (d3.event.pageX) + 'px')
-				.style('top', (d3.event.pageY - 28) + 'px');
-		})
-		.on('mouseout', (d) => {
-			tooltip.transition()
-				.duration(500)
-				.style('opacity', 0);
-		});
+		.on('mouseover', showTooltip)
+		.on("mousemove", moveTooltip)
+		.on('mouseout', hideTooltip);
 };
